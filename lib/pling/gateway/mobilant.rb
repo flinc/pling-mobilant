1e8665c9ed9eb8719f9bf9566bbe4549bed035c8
Konstantin Tennhard <me@t6d.de>
2011-10-25 10:35:28 +0200

    Gateway scaffold

--- lib/pling/gateway/mobilant.rb
+++ lib/pling/gateway/mobilant.rb
@@ -1,6 +1,13 @@
 module Pling
   module Gateway
-    class Mobilant
+    class Mobilant < Base
+      
+      handles :sms, :mobilant
+      
+      def deliver(message, device)
+        message = Pling._convert(message, :message)
+        device  = Pling._convert(device,  :device)
+      end
       
     end
   end
